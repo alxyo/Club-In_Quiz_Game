41ef2fc1b50445645bd38fa93f5458345002b18f
Alex V <110599715+alxyo@users.noreply.github.com>
2023-03-14 00:03:18 +0200

    Update clubin(ver.0.3).py

--- clubin(ver.0.3).py
+++ clubin(ver.0.3).py
@@ -1,21 +1,26 @@
-print("Welcome this is a quiz game called <<How do you enter a club?!>>\n")
+print("Welcome this is a quiz game called <<CLUB-IN>>\n")
 name = input("What is your name?: ")
+age=int(input("How old are you?: "))
 
-print(f"\nWelcome {name}, lets find out how you enter a club!!!\n")
+if age < 18:
+    print("Better go home!!!")
+    exit()
 
-user_input=""
+print(f"\nWelcome {name}, find 10 items in a row to enter the club!!!\n")
 
 while True:
     user_input = input("Are you ready? (Type yes or no): ")
 
     if user_input.lower() == "yes":        
-        print("What item do you hold when you enter a club?")
+        print("\nWhat item do you hold when you enter a club?\n")
         break
     elif user_input.lower() == "no":        
-        print("Better go home")
+        print("\nBye Bye\n")
         exit()
     else:
-        print("Type yes or no") 
+        print("\nType yes or no") 
+
+enter_tries = 1
 
 while True:
     user_item = input("I hold: ")
@@ -24,9 +29,12 @@ while True:
         print("That is your name fool!\n")
         continue
     elif user_item.upper()[0] == name.upper()[0]:
-        print(">>>YOU ENTER!<<<\n")
-        #playing = False
-        continue
+        print("That's a right item ",enter_tries,"/10\n")
+        enter_tries+=1
+        if enter_tries == 11:
+            print("Welcome to the club!\n")
+            exit()
     else:
-        print("--You don't enter!--\n")
+        print("Security Check\n")
+        enter_tries=1
         continue
